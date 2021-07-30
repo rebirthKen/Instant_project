8052a3a55b3bc2777cbceaf45f77afea27b42d7b
chao shi <cshi1017@gmail.com>
2021-07-30 16:31:15 -0500

    Update code

--- app/src/main/java/com/example/instant_project/main/MainActivity.kt
+++ app/src/main/java/com/example/instant_project/main/MainActivity.kt
@@ -1,24 +1,14 @@
 package com.example.instant_project.main
 
-
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import androidx.core.view.isInvisible
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.instant_project.repository.Repository
-import com.example.instant_sample_sushi.R
 import com.example.instant_sample_sushi.databinding.ActivityMainBinding
 import com.stripe.android.paymentsheet.PaymentSheet
 import com.stripe.android.paymentsheet.PaymentSheetResult
-import com.stripe.android.paymentsheet.model.PaymentOption
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.launch
 
 class MainActivity :  AppCompatActivity() {
@@ -32,53 +22,22 @@ class MainActivity :  AppCompatActivity() {
         )
     }
 
-    private val customer: Repository.CheckoutCustomer
-        get() = when (viewBinding.customerRadioGroup.checkedRadioButtonId) {
-            R.id.guest_customer_button -> Repository.CheckoutCustomer.Guest
-            R.id.new_customer_button -> {
-                viewModel.temporaryCustomerId?.let {
-                    Repository.CheckoutCustomer.WithId(it)
-                } ?: Repository.CheckoutCustomer.New
-            }
-            else -> Repository.CheckoutCustomer.Returning
-        }
+    private val customer: Repository.CheckoutCustomer = Repository.CheckoutCustomer.Returning
 
-    private val googlePayConfig: PaymentSheet.GooglePayConfiguration?
-        get() = when (viewBinding.googlePayRadioGroup.checkedRadioButtonId) {
-            R.id.google_pay_on_button -> {
-                PaymentSheet.GooglePayConfiguration(
-                    environment = PaymentSheet.GooglePayConfiguration.Environment.Test,
-                    countryCode = "US"
-                )
-            }
-            else -> null
-        }
+    private val googlePayConfig: PaymentSheet.GooglePayConfiguration? = null
 
-    private val currency: Repository.CheckoutCurrency
-        get() = when (viewBinding.currencyRadioGroup.checkedRadioButtonId) {
-            R.id.currency_usd_button -> Repository.CheckoutCurrency.USD
-            else -> Repository.CheckoutCurrency.EUR
-        }
+    private val currency: Repository.CheckoutCurrency = Repository.CheckoutCurrency.EUR
 
-    private val mode: Repository.CheckoutMode
-        get() = when (viewBinding.modeRadioGroup.checkedRadioButtonId) {
-            R.id.mode_payment_button -> Repository.CheckoutMode.Payment
-            else -> Repository.CheckoutMode.Setup
-        }
+    private val mode: Repository.CheckoutMode = Repository.CheckoutMode.Setup
 
     private lateinit var paymentSheet: PaymentSheet
-    private lateinit var flowController: PaymentSheet.FlowController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
 
         paymentSheet = PaymentSheet(this, ::onPaymentSheetResult)
-        flowController = PaymentSheet.FlowController.create(
-            this,
-            ::onPaymentOption,
-            ::onPaymentSheetResult
-        )
+
 
         viewBinding.reloadButton.setOnClickListener {
             lifecycleScope.launch {
@@ -86,42 +45,11 @@ class MainActivity :  AppCompatActivity() {
             }
         }
 
-        viewBinding.completeCheckoutButton.setOnClickListener {
-            startCompleteCheckout()
-        }
-
-        viewBinding.customCheckoutButton.setOnClickListener {
-            flowController.confirm()
-        }
-
-        viewBinding.paymentMethod.setOnClickListener {
-            flowController.presentPaymentOptions()
-        }
 
         viewModel.status.observe(this) {
             Toast.makeText(this, it, Toast.LENGTH_LONG).show()
         }
 
-        viewModel.inProgress.observe(this) {
-            viewBinding.progressBar.isInvisible = !it
-        }
-
-        viewModel.readyToCheckout.observe(this) { isReady ->
-            if (isReady) {
-                viewBinding.completeCheckoutButton.isEnabled = true
-                configureCustomCheckout()
-            } else {
-                disableViews()
-            }
-        }
-
-        disableViews()
-    }
-
-    private fun disableViews() {
-        viewBinding.completeCheckoutButton.isEnabled = false
-        viewBinding.customCheckoutButton.isEnabled = false
-        viewBinding.paymentMethod.isClickable = false
     }
 
     private fun startCompleteCheckout() {
@@ -140,23 +68,6 @@ class MainActivity :  AppCompatActivity() {
         }
     }
 
-    private fun configureCustomCheckout() {
-        val clientSecret = viewModel.clientSecret.value ?: return
-
-        if (viewModel.checkoutMode == Repository.CheckoutMode.Setup) {
-            flowController.configureWithSetupIntent(
-                clientSecret,
-                makeConfiguration(),
-                ::onConfigured
-            )
-        } else {
-            flowController.configureWithPaymentIntent(
-                clientSecret,
-                makeConfiguration(),
-                ::onConfigured
-            )
-        }
-    }
 
     private fun makeConfiguration(): PaymentSheet.Configuration {
         return PaymentSheet.Configuration(
@@ -166,38 +77,7 @@ class MainActivity :  AppCompatActivity() {
         )
     }
 
-    private fun onConfigured(success: Boolean, error: Throwable?) {
-        if (success) {
-            viewBinding.paymentMethod.isClickable = true
-            onPaymentOption(flowController.getPaymentOption())
-        } else {
-            viewModel.status.value =
-                "Failed to configure PaymentSheetFlowController: ${error?.message}"
-        }
-    }
-
-    private fun onPaymentOption(paymentOption: PaymentOption?) {
-        if (paymentOption != null) {
-            viewBinding.paymentMethod.text = paymentOption.label
-            viewBinding.paymentMethod.setCompoundDrawablesRelativeWithIntrinsicBounds(
-                paymentOption.drawableResourceId,
-                0,
-                0,
-                0
-            )
-            viewBinding.customCheckoutButton.isEnabled = true
-        } else {
-            viewBinding.paymentMethod.setText("select")
-            viewBinding.paymentMethod.setCompoundDrawables(null, null, null, null)
-            viewBinding.customCheckoutButton.isEnabled = false
-        }
-    }
-
     private fun onPaymentSheetResult(paymentResult: PaymentSheetResult) {
-        if (paymentResult !is PaymentSheetResult.Canceled) {
-            disableViews()
-        }
-
         viewModel.status.value = paymentResult.toString()
     }
 
